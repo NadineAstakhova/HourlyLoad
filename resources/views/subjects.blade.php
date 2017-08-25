fd804530240132f9855c8000446941b94f8fae6d
NadineAstakhova <nadine.astakhova@gmail.com>
2017-08-25 22:41:19 +0300

    show subject with free hours

--- resources/views/subjects.blade.php
+++ resources/views/subjects.blade.php
@@ -11,6 +11,7 @@
             <thead>
                 <tr>
                     <th>Предмет</th>
+                    <th>Тип работы</th>
                     <th>Свободные часы</th>
                     <th>Курс</th>
                     <th>Семестр</th>
@@ -18,13 +19,17 @@
             </thead>
             <tbody>
                 @foreach ($sub as $s)
+                    @php $freeHours = \HoursLoad\Subject::getFreeHours($s->idLoadSub, $s->hours)@endphp
+                    @if ($freeHours != 0)
                     <tr>
                         <td>{{$s->name}}</td>
                         <td>{{$s->type}}</td>
+                        <td>{{$freeHours}}</td>
                         <td>{{$s->course}}</td>
                         <td>{{$s->term}}</td>
                         <td><a href="{{url("addform/$idProf/$s->idSubjects")}}">Add</a></td>
                     </tr>
+                    @endif
                 @endforeach
             </tbody>
         </table>
