9f812c4ebfdb6252b745a8dd5bd3b3ce766d6f4f
NadineAstakhova <nadine.astakhova@gmail.com>
2017-11-17 12:31:18 +0200

    validate pass update form

--- resources/views/resetpass.blade.php
+++ resources/views/resetpass.blade.php
@@ -5,24 +5,45 @@
 <h3>Для изменения пароля нужно заполнить следующие поля:</h3>
     {!! Form::label('cur_password', 'Текущий пароль') !!}
 
-    {!! Form::password('cur_password', ['class' => 'form-control']) !!}
+    {!! Form::password('cur_password', ['class' => 'form-control', 'required' => 'true']) !!}
     @if(Session::has('error'))
-       {{Session::get("error")}}
+       <span class = "error"> {{Session::get("error")}} </span>
         <br>
     @endif
 
     {!! Form::label('new_password', 'Новый пароль') !!}
-    {!! Form::password('new_password', ['class' => 'form-control']) !!}
+    {!! Form::password('new_password', ['class' => 'form-control', 'required' => 'true', 'id' => 'passNew', 'minlength' => 6]) !!}
 
     {!! Form::label('password_confirm', 'Повторите пароль') !!}
-    {!! Form::password('password_confirm', ['class' => 'form-control']) !!}
+    {!! Form::password('password_confirm', ['class' => 'form-control', 'id' => 'passConf', 'required' => 'true', 'minlength' => 6]) !!}
+    <span class = "error" id="conf"></span>
     <br>
-    {!! Form::submit('Save', ['class' => 'btn btn-default', 'id' => 'btn']) !!}
+    {!! Form::submit('Save', ['class' => 'btn btn-default', 'id' => 'btn', 'disabled' => 'true']) !!}
 
     <a class="btn btn-default btn-close" href="{{ url()->previous() }}">Cancel</a>
 
     {!! Form::close() !!}
 
+<script>
+    $('#passConf').keyup(function() {
+       formval();
+    });
+    $('#passNew').keyup(function() {
+        formval();
+    });
 
+    function formval() {
+        if($('#passNew').val() != $('#passConf').val()) {
+            $('#conf').html('Пароли не совпдают');
+            $('#btn').prop( "disabled", true );
+        }
+        else {
+            $('#conf').html('');
+            $('#btn').prop( "disabled", false );
+
+        }
+    }
+
+</script>
 
 @endsection
